04edc39d7761b250fea9749e38513092b11f01f2
jontcont <52921053+JontCont@users.noreply.github.com>
2022-07-19 22:44:45 +0800

    initial data

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs{
+    public class ChatHub : Hub{
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        } // SendMessage
+    }//class : ChartHub    
+}// namespace : SignalRChat
